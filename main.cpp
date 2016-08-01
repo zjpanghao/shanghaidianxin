7ee7f6ba24800ce7aca626f084569964af715001
panghao <panghao@localhost.(none)>
2016-08-01 11:44:28 +0800

    1. add kafka server ip config

--- main.cpp
+++ main.cpp
@@ -12,15 +12,20 @@
 #include "tele_task_producer.h"
 #include "tele_task_consumer.h"
 using namespace std;
+enum TeleCommand{
+	INVALID_COMMAND = 0,
+	KAFKA_COMMAND = 1,
+};
 
 wrapper_Info *test_info_g;
-static int init_kafka(wrapper_Info *test_info)
+
+static int init_kafka(wrapper_Info *test_info, const char* kafka_server)
 {
 	int partition = 0;
   const char* topic1 = "test";
   const char* topic2 = "stock_heat";
 	/*init kafka*/
-	if (PRODUCER_INIT_SUCCESS == producer_init(partition, topic2, "222.73.34.101:9092", &test_msg_delivered, test_info)) {
+	if (PRODUCER_INIT_SUCCESS == producer_init(partition, topic2, kafka_server, &test_msg_delivered, test_info)) {
 		printf("producer init success!\n");
 		return 0;
 	}
@@ -39,8 +44,21 @@ int main(int argc, char* argv[])
 		pthread_t tid[10] = {0};
 		int code = 0;
 		int i;
+                TeleCommand cmd;
+                const char *kafka_server;
+	for (int i = 0; i < argc; i++) {
+		switch (cmd) {
+		  case KAFKA_COMMAND:
+		  	kafka_server = argv[i];
+			  break;
+		}
+		cmd = INVALID_COMMAND;
+		if (strcmp(argv[i], "-k") == 0) {
+			cmd = KAFKA_COMMAND;
+		} 
+         }
 		test_info_g = new wrapper_Info;
-		init_kafka(test_info_g);
+		init_kafka(test_info_g, kafka_server);
     pthread_t producerid;
     std::string url_fetch_token = "http://61.129.39.71/telecom-dmp/getToken?apiKey=98f5103019170612fd3a486e3d872c48&sign=6a653929c81a24ba14e41e25b6047e5dec55e76e";
     TeleTaskProducer::GetInstance()->Init(url_fetch_token);
